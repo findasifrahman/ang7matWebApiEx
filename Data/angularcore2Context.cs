5338ee1725808d8e1c810b454b5d302ec4e4fc8a
findasifrahman <findasifrahman@gmail.com>
2019-04-09 18:20:10 +0600

    tenth commit

---
+++ Data/angularcore2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace angularcore2.Models
+{
+    public class angularcore2Context : DbContext
+    {
+        public angularcore2Context (DbContextOptions<angularcore2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<angularcore2.Models.Products> Products { get; set; }
+        public DbSet<angularcore2.Models.LoginModels> LoginModels { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace angularcore2.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public string productGroup { get; set; }
+        public string productTitle { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+        public string price { get; set; }
+        public string description { get; set; }
+    }
+}
